358b2fc47b5cfc05ec53c44e4a845f875e85b9af
Sigri <sigri44@hotmail.fr>
2024-08-29 18:27:13 +0200

    Add redeem line

--- script.js
+++ script.js
@@ -246,6 +246,19 @@ async function getPoolPrices(token) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       originalPrice = await response.json();
+      results.push({
+        chain: 'ethereum',
+        protocol: 'redeem',
+        address: '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0',
+        dexScreenerPrice: originalPrice.toFixed(5),
+        dexScreenerDiff: '',
+        dexScreenerLiquidity: '',
+        geckoTerminalPrice:  '',
+        geckoTerminalDiff: '',
+        geckoTerminalLiquidity: '',
+        dexScreenerLink: '',
+        geckoTerminalLink: ''
+      });
     } catch (error) {
       console.error('Error fetching Lido redeem price:', error);
     }
